8023a8c27d3c490ea9d1dc9a3ab4027a644e224d
Ijas Ahammed <ahammedijas.official@gmail.com>
2024-04-03 07:50:59 +0530

    Copy to clipboard and delete from localstorage done

--- synopz/src/components/Demo.jsx
+++ synopz/src/components/Demo.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import LinkIcon from "@mui/icons-material/Link";
 import DoneIcon from "@mui/icons-material/Done";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import loader from "../assets/loader.svg";
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
@@ -11,6 +13,7 @@ const Demo = () => {
   });
 
   const [allArticles, setAllArticles] = useState([]);
+  const [copy, setCopy] = useState("");
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
@@ -36,6 +39,20 @@ const Demo = () => {
 
       localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
+  };
+
+  const handleRemoveArticle = (urlToRemove) => {
+    const updatedArticles = allArticles.filter(
+      (article) => article.url !== urlToRemove
+    );
+    setAllArticles(updatedArticles);
+    localStorage.setItem("articles", JSON.stringify(updatedArticles));
+  };
+
+  const handleCopy = (copyUrl) => {
+    setCopy(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopy(false), 3000);
   };
 
   return (
@@ -69,15 +86,54 @@ const Demo = () => {
               onClick={() => setArticle(item)}
               className="link_card"
             >
-              <div className="copy_btn">
-                <ContentCopyIcon sx={{ fontSize: 16 }} />
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                {copy === item.url ? (
+                  <DoneIcon sx={{ fontSize: 16 }} />
+                ) : (
+                  <ContentCopyIcon sx={{ fontSize: 16 }} />
+                )}
               </div>
               <p className="flex-1 font-satoshi text-blue-400 font-medium text-sm truncate">
                 {item.url}
               </p>
+              <div
+                onClick={(e) => {
+                  e.stopPropagation(); // Prevent click on remove button from triggering the click on link_card
+                  handleRemoveArticle(item.url);
+                }}
+              >
+                <DeleteOutlineIcon />
+              </div>
             </div>
           ))}
         </div>
+      </div>
+
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
